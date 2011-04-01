018a949063fccdc6b68083e586bfac4854d60c3e
Jamie Lentin <jm+plone@lentin.co.uk>
2011-04-01 16:07:06 +0000

    _validate still used when import/exporting, fix up code so it works
    svn path=/collective.z3cform.datagridfield/trunk/; revision=236809

--- collective/z3cform/datagridfield/row.py
+++ collective/z3cform/datagridfield/row.py
@@ -19,21 +19,23 @@ class DictRow(Object):
         super(DictRow, self).__init__(schema, **kw)
 
     def _validate(self, value):
-        super(DictRow, self)._validate(value)
+        #HACK: Can't call the super, since it'll check to see if we provide DictRow.
+        #We're only a dict, so we can't.
+        # super(DictRow, self)._validate(value)
 
         # Validate the dict against the schema
         # Pass 1 - ensure fields are present
         errors = []
-        for field in getFields(self.schema):
-            if field.__name__ not in value:
-                errors.append(AttributeNotFoundError(field.__name__, self.schema))
+        for field_name in getFields(self.schema).keys():
+            if field_name not in value:
+                errors.append(AttributeNotFoundError(field_name, self.schema))
 
         if errors:
             raise WrongContainedType(errors, self.__name__)
 
         # Pass 2 - Ensure fields are valid
-        for field in getFields(self.schema):
-            field.validate(value)
+        for field_name, field_type in getFields(self.schema).items():
+            field_type.validate(value[field_name])
 
     def set(self, object, value):
         # Override Object field logic
