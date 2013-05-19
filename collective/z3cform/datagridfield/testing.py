7d8204c552b5ff625ba33fddf10a3b1c2c8a3a59
Daniel Widerin <daniel@widerin.net>
2013-05-20 01:53:26 +0200

    Try to fix that grok errors on travis.

--- collective/z3cform/datagridfield/testing.py
+++ collective/z3cform/datagridfield/testing.py
@@ -1,17 +1,32 @@
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import FunctionalTesting
+from zope.configuration import xmlconfig
 
 
 class Fixture(PloneSandboxLayer):
+
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
+
+        import five.grok
+        xmlconfig.file('meta.zcml', five.grok,
+                       context=configurationContext)
+
         import collective.z3cform.datagridfield
-        self.loadZCML(package=collective.z3cform.datagridfield)
+        xmlconfig.file(
+            'configure.zcml',
+            collective.z3cform.datagridfield,
+            context=configurationContext
+        )
 
         import collective.z3cform.datagridfield_demo
-        self.loadZCML(package=collective.z3cform.datagridfield_demo)
+        xmlconfig.file(
+            'configure.zcml',
+            collective.z3cform.datagridfield_demo,
+            context=configurationContext
+        )
 
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, 'collective.z3cform.datagridfield:default')
