collective/collective.z3cform.datagridfield

commit 9c9000102c8b47b27a609678d9269a3db554e15d

pyflake cleanup

collective/z3cform/datagridfield/datagridfield.py
@@ -29,8 +29,7 @@ from interfaces import IDataGridField
 
 try:
     # support plone.autoform directives within the row schema
-    import plone.autoform
-    from autoform import AutoExtensibleSubForm as ObjectSubForm
+    from pautoform import AutoExtensibleSubForm as ObjectSubForm
     from autoform import AutoExtensibleSubformAdapter as SubformAdapter
 except ImportError:
     # Plain z3c ObjectSubForm support
@@ -38,7 +37,7 @@ except ImportError:
     from z3c.form.object import SubformAdapter
 
 
-#------------[ Main Widget ]-----------------------------------------------
+# ------------[ Main Widget ]-----------------------------------------------
 
 class DataGridField(MultiWidget):
     """This grid should be applied to an schema.List item which has
@@ -101,7 +100,7 @@ class DataGridField(MultiWidget):
                 widget.setErrors = True
         else:
             widget = zope.component.getMultiAdapter((valueType, self.request),
-                interfaces.IFieldWidget)
+                                                    interfaces.IFieldWidget)
 
         return widget
 
@@ -119,7 +118,7 @@ class DataGridField(MultiWidget):
 
         widget.mode = self.mode
         widget.klass = 'datagridwidget-row'
-        #set widget.form (objectwidget needs this)
+        # set widget.form (objectwidget needs this)
         if interfaces.IFormAware.providedBy(self):
             widget.form = self.form
             zope.interface.alsoProvides(
@@ -140,8 +139,8 @@ class DataGridField(MultiWidget):
             # these are not "real" elements of the MultiWidget
             # and confuse updateWidgets method if len(self.widgets) changes
             # This is relevant for nested datagridfields.
-            self.widgets = [ w for w in self.widgets
-                             if not (w.id.endswith('AA') or w.id.endswith('TT')) ]
+            self.widgets = [w for w in self.widgets
+                            if not (w.id.endswith('AA') or w.id.endswith('TT'))]
 
         # if the field has configuration data set - copy it
         super(DataGridField, self).updateWidgets()
@@ -208,7 +207,7 @@ class GridDataConverter(BaseDataConverter):
         return value
 
 
-#------------[ Support for each line ]-----------------------------------------
+# ------------[ Support for each line ]-----------------------------------------
 
 class DataGridFieldObject(ObjectWidget):
 
@@ -230,41 +229,43 @@ class DataGridFieldObject(ObjectWidget):
            want to allow a field to handle a sub-set of the schema. I
            filter on the subform.fields
         """
+        return property(datagrid_field_get, datagrid_field_set)
 
-        def get(self):
-            # value (get) cannot raise an exception, then we return
-            # insane values
+
+def datagrid_field_get(self):
+    # value (get) cannot raise an exception, then we return
+    # insane values
+    try:
+        return self.extract()
+    except MultipleErrors:
+        value = {}
+        active_names = self.subform.fields.keys()
+        for name in getFieldNames(self.field.schema):
+            if name not in active_names:
+                continue
+            widget = self.subform.widgets[name]
+            widget_value = widget.value
             try:
-                return self.extract()
-            except MultipleErrors:
-                value = {}
-                active_names = self.subform.fields.keys()
-                for name in getFieldNames(self.field.schema):
-                    if name in active_names:
-                        widget = self.subform.widgets[name]
-                        widget_value = widget.value
-                        try:
-                            converter = interfaces.IDataConverter(widget)
-                            value[name] = converter.toFieldValue(widget_value)
-                        except (FormatterValidationError,
-                                zope.schema.interfaces.ValidationError,
-                                ValueError):
-                            value[name] = widget_value
-                return value
+                converter = interfaces.IDataConverter(widget)
+                value[name] = converter.toFieldValue(widget_value)
+            except (FormatterValidationError,
+                    zope.schema.interfaces.ValidationError,
+                    ValueError):
+                value[name] = widget_value
+    return value
 
-        def set(self, value):
-            self._value = value
-            self.updateWidgets()
 
-            # ensure that we apply our new values to the widgets
-            if value is not interfaces.NO_VALUE:
-                active_names = self.subform.fields.keys()
-                for name in getFieldNames(self.field.schema):
-                    if name in active_names:
-                        self.applyValue(self.subform.widgets[name],
-                                    value.get(name, interfaces.NO_VALUE))
+def datagrid_field_set(self, value):
+    self._value = value
+    self.updateWidgets()
 
-        return property(get, set)
+    # ensure that we apply our new values to the widgets
+    if value is not interfaces.NO_VALUE:
+        active_names = self.subform.fields.keys()
+        for name in getFieldNames(self.field.schema):
+            if name in active_names:
+                self.applyValue(self.subform.widgets[name],
+                                value.get(name, interfaces.NO_VALUE))
 
 
 @zope.component.adapter(zope.schema.interfaces.IField, interfaces.IFormLayer)
@@ -274,7 +275,7 @@ def DataGridFieldObjectFactory(field, request):
     return FieldWidget(field, DataGridFieldObject(request))
 
 
-#------------[ Form to draw the line ]-----------------------------------------
+# ------------[ Form to draw the line ]-----------------------------------------
 
 
 class DataGridFieldObjectSubForm(ObjectSubForm):
@@ -356,11 +357,11 @@ class DataGridValidator(SimpleFieldValidator):
     """
     zope.interface.implements(IValidator)
     zope.component.adapts(
-              zope.interface.Interface,
-              zope.interface.Interface,
-              zope.interface.Interface,  # Form
-              IList,                     # field
-              DataGridField)             # widget
+        zope.interface.Interface,
+        zope.interface.Interface,
+        zope.interface.Interface,  # Form
+        IList,                     # field
+        DataGridField)             # widget
 
     def validate(self, value, force=False):
         """

.travis.yml
@@ -19,7 +19,7 @@ matrix:
     - python: 2.6
       env: PLONE_VERSION=4.0
 install:
-  - sed -ie "s#travis-4.x.cfg#travis-$PLONE_VERSION.x.cfg#" travis.cfg
+  - sed -ie "s#travis-5.x.cfg#travis-$PLONE_VERSION.x.cfg#" travis.cfg
   - mkdir -p buildout-cache/{eggs,downloads}
   - virtualenv .
   - bin/pip install --upgrade pip zc.buildout

buildout.cfg
@@ -1,6 +1,7 @@
 [buildout]
 extends =
-    test-4.3.x.cfg
+    test-5.0.x.cfg
+    https://raw.github.com/collective/buildout.plonetest/master/qa.cfg
 
 parts +=
     omelette
@@ -36,3 +37,7 @@ packages = ${instance:location}/lib/python ./
 [scripts]
 recipe = zc.recipe.egg
 eggs = zest.releaser
+
+[code-analysis]
+recipe = plone.recipe.codeanalysis
+directory = ${buildout:directory}/collective

collective/z3cform/datagridfield/__init__.py
@@ -1,14 +1,14 @@
 from zope.i18nmessageid import MessageFactory
 
+from interfaces import IDataGridField  # noqa # pylint: disable=unused-import
+from interfaces import IRow  # noqa # pylint: disable=unused-import
+
+from datagridfield import DataGridFieldFactory  # noqa # pylint: disable=unused-import
+from datagridfield import DataGridField  # noqa # pylint: disable=unused-import
+
+from blockdatagridfield import BlockDataGridField  # noqa # pylint: disable=unused-import
+from blockdatagridfield import BlockDataGridFieldFactory  # noqa # pylint: disable=unused-import
+
+from row import DictRow  # noqa # pylint: disable=unused-import
+
 _ = MessageFactory('collective.z3cform.datagridfield')
-
-from interfaces import IDataGridField
-from interfaces import IRow
-
-from datagridfield import DataGridFieldFactory
-from datagridfield import DataGridField
-
-from blockdatagridfield import BlockDataGridField
-from blockdatagridfield import BlockDataGridFieldFactory
-
-from row import DictRow

collective/z3cform/datagridfield/autoform.py
@@ -36,7 +36,6 @@ class AutoExtensibleSubForm(AutoExtensibleForm, ObjectSubForm):
         pass
 
     def updateWidgets(self):
-        #AutoExtensibleForm.updateWidgets(self)
         ObjectSubForm.updateWidgets(self)
 
     def update(self):

collective/z3cform/datagridfield/converters.py
@@ -1,4 +1,5 @@
-from transmogrify.dexterity.interfaces import ISerializer, IDeserializer
+from transmogrify.dexterity.converters import DefaultSerializer
+from transmogrify.dexterity.interfaces import ISerializer
 
 from zope.component import adapts
 from zope.interface import implements
@@ -6,6 +7,7 @@ from zope.interface import implements
 from row import DictRow
 
 from zope.schema import getFields
+
 
 class DictRowSerializer:
     """Serializer to allow transmogrify.dexterity to write the dict. A custom
@@ -17,8 +19,9 @@ class DictRowSerializer:
     def __init__(self, field):
         self.field = field
 
-    def _serializer(self,field_type):
-        if field_type is not None: return ISerializer(field_type)
+    def _serializer(self, field_type):
+        if field_type is not None:
+                return ISerializer(field_type)
         return DefaultSerializer()
 
     def __call__(self, value, filestore, extra=None):

collective/z3cform/datagridfield/interfaces.py
@@ -3,8 +3,10 @@ from zope.schema.interfaces import IObject
 from zope.schema.interfaces import ValidationError
 from z3c.form.interfaces import IMultiWidget
 
+
 class IDataGridField(IMultiWidget):
     """Grid widget."""
+
 
 class AttributeNotFoundError(ValidationError):
     """An attribute is missing from the class"""
@@ -15,8 +17,6 @@ class AttributeNotFoundError(ValidationError):
         self.__doc__ = u'Missing Field %s required by schema %s' % (fieldname, schema)
 
 
-
 class IRow(IObject):
     """A row. The schema defines dict keys.
     """
-

setup.py
@@ -39,6 +39,7 @@ setup(name='collective.z3cform.datagridfield',
               'plone.app.testing',
               'collective.z3cform.datagridfield_demo',
               'unittest2',
+              'transmogrify.dexterity',
           ]
       },
       entry_points="""

commit 5341d9a8f8e14a9287bfba619ce750a2acd7302a

Import AutoExtensibleSubForm from autoform instead of from pautoform does not exist and instead reverts back to using AutoExtensibleSubForm from z3c.form.object.
Issue introduced in:
https://github.com/collective/collective.z3cform.datagridfield/commit/9c9000102c8b47b27a609678d9269a3db554e15d#diff-552dc8feb0d191a102754ec6b6190c0aL33

collective/z3cform/datagridfield/datagridfield.py
@@ -28,11 +28,14 @@ from plone.app.z3cform.interfaces import IPloneFormLayer
 from interfaces import IDataGridField
 
 try:
-    # support plone.autoform directives within the row schema
-    from pautoform import AutoExtensibleSubForm as ObjectSubForm
-    from autoform import AutoExtensibleSubformAdapter as SubformAdapter
+    from plone import autoform as has_autoform  # noqa # pylint: disable=unused-import
 except ImportError:
-    # Plain z3c ObjectSubForm support
+    has_autoform = None
+if has_autoform:
+    # support plone.autoform directives within the row schema
+    from autoform import AutoExtensibleSubForm as ObjectSubForm
+    from autoform import AutoExtensibleSubformAdapter as SubformAdapter
+else:
     from z3c.form.object import ObjectSubForm
     from z3c.form.object import SubformAdapter
 