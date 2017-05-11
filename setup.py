e0cc06ea365e2296f621d46c9a201bccfe927b09
Jens W. Klein <jk@kleinundpartner.at>
2017-05-11 13:17:58 +0200

    cleanup setup.py, add classifiers

--- setup.py
+++ setup.py
@@ -1,49 +1,62 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
 
-version = '1.2.1.dev0'
 
-setup(name='collective.z3cform.datagridfield',
-      version=version,
-      description="Version of DataGridField for use with Dexterity / z3c.form",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-      ],
-      keywords='Plone, Dexterity, z3c.form',
-      author='Kevin Gill',
-      author_email='kevin@movieextras.ie',
-      url='https://github.com/collective/collective.z3cform.datagridfield',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.z3cform'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'plone.app.z3cform',
-          'setuptools',
-          'z3c.form >=2.4.3dev',
-          'zope.component',
-          'zope.i18nmessageid',
-          'zope.interface',
-          'zope.schema',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'collective.z3cform.datagridfield_demo',
-              'unittest2',
-              'transmogrify.dexterity',
-          ]
-      },
-      entry_points="""
-      # -*- Entry points: -*-
+version = '1.3.0.dev0'
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='collective.z3cform.datagridfield',
+    version=version,
+    description="Version of DataGridField for use with Dexterity / z3c.form",
+    long_description=(
+        open("README.rst").read() +
+        '\n' +
+        open("CHANGES.rst").read()
+    ),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+    ],
+    keywords='Plone, Dexterity, z3c.form',
+    author='Kevin Gill',
+    author_email='kevin@movieextras.ie',
+    url='https://github.com/collective/collective.z3cform.datagridfield',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.z3cform'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.app.z3cform',
+        'plone.autoform',
+        'setuptools',
+        'z3c.form >=2.4.3dev',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.schema',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'collective.z3cform.datagridfield_demo',
+            'unittest2',
+            'transmogrify.dexterity',
+        ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
