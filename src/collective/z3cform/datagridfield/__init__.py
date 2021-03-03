6e11a31fdea4012fab5a921740bcaa6ce51c3638
Johannes Raggam <thetetet@gmail.com>
2021-03-03 11:59:03 +0100

    Cleanup

--- src/collective/z3cform/datagridfield/__init__.py
+++ src/collective/z3cform/datagridfield/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-# Preven circular imports
-# from .blockdatagridfield import BlockDataGridField  # noqa: F401
-# from .blockdatagridfield import BlockDataGridFieldFactory  # noqa: F401
-# from .datagridfield import DataGridField  # noqa: F401
-# from .datagridfield import DataGridFieldFactory  # noqa: F401
-# from .interfaces import IDataGridField  # noqa: F401
-# from .interfaces import IRow  # noqa: F401
-# from .row import DictRow  # noqa: F401
 from zope.i18nmessageid import MessageFactory
 
 
