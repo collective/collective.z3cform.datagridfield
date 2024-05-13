834a14b27444ae1c38cff2ba221c7e602ad24acc
Peter Mathis <peter.mathis@kombinat.at>
2024-05-13 11:51:37 +0200

    update demo package with supermodel xml declaration

--- src/collective/z3cform/datagridfield/demo/content.py
+++ src/collective/z3cform/datagridfield/demo/content.py
@@ -1,62 +1,31 @@
-from collective.z3cform.datagridfield.datagridfield import \
-    DataGridFieldWidgetFactory
-from collective.z3cform.datagridfield.row import DictRow
-from plone import schema
-from plone.app.z3cform.widgets.select import AjaxSelectFieldWidget
-from plone.autoform import directives
+# from collective.z3cform.datagridfield.datagridfield import DataGridFieldWidgetFactory
+# from collective.z3cform.datagridfield.demo.interfaces import ITableRow
+# from collective.z3cform.datagridfield.row import DictRow
+# from plone import schema
+# from plone.autoform import directives
 from plone.dexterity.content import Item
-from plone.namedfile import field as namedfile
 from plone.supermodel import model
-from z3c.relationfield.schema import RelationChoice
-from z3c.relationfield.schema import RelationList
 from zope.interface import implementer
-from zope.interface import Interface
-from zope.schema.vocabulary import SimpleVocabulary
-
-
-class ITableRow(Interface):
-    col1 = schema.TextLine(title="Column1")
-    col2 = schema.Choice(vocabulary=SimpleVocabulary.fromValues(["yes", "no"]), required=False)
-    links = RelationList(
-        title="Related Links",
-        value_type=RelationChoice(
-            vocabulary="plone.app.vocabularies.Catalog",
-        ),
-        required=False,
-    )
-
-    tags = schema.Tuple(
-        title="Tags",
-        value_type=schema.TextLine(),
-        required=False,
-    )
-
-    directives.widget(
-        "tags",
-        AjaxSelectFieldWidget,
-        vocabulary="plone.app.vocabularies.Keywords",
-    )
-
-    image = namedfile.NamedBlobImage(
-        title="Image",
-        required=False,
-    )
 
 
 class IDGFTest(model.Schema):
     """Marker interface and Dexterity Python Schema for DGFTest"""
 
-    tabular_field = schema.List(
-        title="datagridfield",
-        value_type=DictRow(schema=ITableRow),
-        required=False,
-    )
-    directives.widget(
-        "tabular_field",
-        DataGridFieldWidgetFactory,
-        auto_append=True,
-        allow_reorder=True,
-    )
+    # You can define the fields in the schema interface directly or load an XML
+    # file ... see below
+    # tabular_field = schema.List(
+    #     title="datagridfield",
+    #     value_type=DictRow(schema=ITableRow),
+    #     required=False,
+    # )
+    # directives.widget(
+    #     "tabular_field",
+    #     DataGridFieldWidgetFactory,
+    #     auto_append=True,
+    #     allow_reorder=True,
+    # )
+
+    model.load('dgftest.xml')
 
 
 @implementer(IDGFTest)
